defb0370c034a91c9b62c8f95f82ca4f4250df06
Andrea R <amrdev08@gmail.com>
2016-10-15 22:12:52 -0400

    uploading images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,28 @@
 
 
 exports.friends = [{
-    name:"Ahmed",
+	name:"Ahmed",
     photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores: [5,1,4,4,5,1,2,5,4,1]   
-  }, 
-  {
-  
-    name: "Leonardo",
-    photo: "",
-    scores: [1,2,3,4,5,4,3,2,1,2]
-  }
+	}, 
 
+	{
+    name: "Leonardo",
+  	photo: "leo.jpg",
+    scores: [1,2,3,4,5,4,3,2,1,2]
+	},
+
+	{
+    name: "Matt",
+    photo: "matt.jpg",
+    scores: [1,2,3,4,5,4,3,2,1,2]
+	},
+
+	{
+    name: "Jack",
+    photo: "jack-black-2.jpg",
+    scores: [1,2,3,4,5,4,3,2,1,2]
+  	}
+  	
 ];
 
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -6,8 +6,6 @@ var friendsFile = require("../data/friends.js")
 var friendsArray = friendsFile.friends
 
 
-
-
 var ApiRoutes = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
@@ -18,15 +16,16 @@ var ApiRoutes = function (app) {
   app.get('/api/friends', function (req, res) {
     res.json(friendsArray);
   });
-console.log(friendsArray)
-  // app.post('/api/friends', function (req, res) {
-  //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  //   // It will do this by sending out the value "true" have a table
-    
-  //     friendsArray.push(req.body);
 
-  //     res.json(true); // KEY LINE
-  //   } 
+  // app.post('/api/friends', function (req, res) {
+  // //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+  // //   // It will do this by sending out the value "true" have a table
+    
+  // //     friendsArray.push(req.body);
+
+  // //     res.json(true); // KEY LINE
+  // //   } 
   // });
 };
+
 module.exports = ApiRoutes;
